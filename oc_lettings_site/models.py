41bfcbc4d3d3a29b4128c1a08c6961257be1508e
Thomas Poupinet <th.poupinet@free.fr>
2023-02-19 16:54:39 +0100

    reformating code

--- oc_lettings_site/models.py
+++ oc_lettings_site/models.py
@@ -8,11 +8,16 @@ class Address(models.Model):
     street = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
     state = models.CharField(max_length=2, validators=[MinLengthValidator(2)])
-    zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
-    country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
+    zip_code = models.PositiveIntegerField(
+        validators=[MaxValueValidator(99999)])
+    country_iso_code = models.CharField(max_length=3, validators=[
+        MinLengthValidator(3)])
 
     def __str__(self):
         return f'{self.number} {self.street}'
+
+    class Meta:
+        verbose_name_plural = "Addresses"
 
 
 class Letting(models.Model):
@@ -22,6 +27,9 @@ class Letting(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name_plural = "Lettings"
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -29,3 +37,6 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    class Meta:
+        verbose_name_plural = "Profiles"
