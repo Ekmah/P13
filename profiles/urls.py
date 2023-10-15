3773ede5b3dd0bc90c68759a020f92642e92f09d
Thomas Poupinet <th.poupinet@free.fr>
2023-10-15 16:47:16 +0200

    configs: sentry? ah forgot that part...

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 
 from . import views
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('', views.index, name='profiles_index'),
     path('sentry-debug/', trigger_error),
