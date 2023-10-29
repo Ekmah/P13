61f5ae210078f9b2ee3fd7d4910df84b8220f412
Thomas Poupinet <th.poupinet@free.fr>
2023-10-29 17:31:48 +0100

    patch settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
